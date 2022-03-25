d50e529a6a5bd3ffdcca9842922f1725a15f0edd
Sebas R <sebasrg0505@gmail.com>
2022-03-24 23:06:54 -0500

    feat: login frontend

---
+++ ui/login-service/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import 'flowbite'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons'
+
+localStorage.theme = 'dark'
+library.add(faEye, faEyeSlash)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
